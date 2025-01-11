c52785ed10f9d05b07c5afd26797100bd49fee0d
toxidxd <toxid13@yandex.ru>
2025-01-11 11:56:12 +0300

    code cleanup

--- main.py
+++ main.py
@@ -1,19 +1,59 @@
 import random
+import time
 
-mult_table = list()
-task_table = list()
 
-for x in range(2, 10):
-    for y in range (2, 10):
-        # print(f'{x} * {y} = ')
-        mult_table.append(f'{x} * {y} = ')
+def gen_mult_table() -> list[list[int]]:
+    result = list()
+    for x in range(2, 10):
+        for y in range(2, 10):
+            result.append([x, y])
 
-for _ in range(32):
-    # print(tab[random.randint(0, len(tab) - 1)])
-    task_table.append(mult_table[random.randint(0, len(mult_table) - 1)])
+    return result
 
-with open('task.txt', 'w', encoding='utf-8') as f:
-    for line in task_table:
-        f.write(line + '\n')
 
-print('File wrote.')
+def gen_task_table(table: list[list[int]], count: int) -> list[list[int]]:
+    result = list()
+    indexes = list(range(len(table)))
+    random.shuffle(indexes)
+    indexes = indexes[:count]
+    for i_index in indexes:
+        result.append(table[i_index])
+
+    return result
+
+
+def main():
+    print('Добро пожаловать в тренажер таблицы умножения!')
+
+    mult_table = gen_mult_table()
+    errors_count = 0
+
+    tasks_count = int(input('Количество примеров: '))
+    task_table = gen_task_table(mult_table, tasks_count)
+
+    start = time.time()
+    for num, task in enumerate(task_table):
+        while True:
+            try:
+                answer = int(input(f'{num + 1}) {task[0]} * {task[1]} = '))
+                if answer == task[0] * task[1]:
+                    print(f'Правильно!')
+                    break
+                else:
+                    errors_count += 1
+                    print('Неправильно!')
+
+            except ValueError as exc:
+                print('Ответ должен быть числом!')
+
+    end = time.time()
+    elapsed_time = (end - start) // 60
+
+    print(f'Молодец, ты справился!\nКоличество ошибок: {errors_count}')
+    print(f'Время выполнения: {elapsed_time} минут')
+
+    while True:
+        pass
+
+if __name__ == '__main__':
+    main()
