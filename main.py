597c9877364356c96cb869d5c364d43700462c7e
toxidxd <toxid13@yandex.ru>
2025-01-11 12:38:31 +0300

    added mixed mode

--- main.py
+++ main.py
@@ -20,9 +20,51 @@ def gen_task_table(table: list[list[int]], count: int) -> list[list[int]]:
 
     return result
 
+def mix_tasks():
+    tasks_count = int(input('Количество примеров: '))
+
+    div_table = gen_mult_table()
+    errors_count = 0
+    task_table = gen_task_table(div_table, tasks_count)
+
+    start = time.time()
+    for num, task in enumerate(task_table):
+        while True:
+            try:
+                mult_or_div = random.randint(0, 1)
+
+                if mult_or_div == 0:
+                    answer = int(input(f'{num + 1}) {task[0]} * {task[1]} = '))
+                    if answer == task[2]:
+                        print(f'Правильно!')
+                        break
+                    else:
+                        errors_count += 1
+                        print('Неправильно!')
+
+                else:
+                    answer = int(input(f'{num + 1}) {task[2]} / {task[0]} = '))
+                    if answer == task[2] / task[0]:
+                        print(f'Правильно!')
+                        break
+                    else:
+                        errors_count += 1
+                        print('Неправильно!')
+
+
+            except ValueError as exc:
+                print('Ответ должен быть числом!')
+
+    end = time.time()
+    elapsed_time = (end - start) // 60
+
+    well_done(errors_count, elapsed_time)
+
+    while True:
+        pass
+
 
 def multiplication():
-    print('Добро пожаловать в тренажер таблицы умножения!')
     tasks_count = int(input('Количество примеров: '))
 
     mult_table = gen_mult_table()
@@ -54,7 +96,6 @@ def multiplication():
 
 
 def division():
-    print('Добро пожаловать в тренажер деления!')
     tasks_count = int(input('Количество примеров: '))
 
     div_table = gen_mult_table()
@@ -92,12 +133,14 @@ def well_done(errors, time):
 
 def main():
     print('Добро пожаловать в тренажер таблицы умножения и деления!')
-    print('Выберите задание \n\t1 - умножение\n\t2 - деление')
+    print('Выберите задание \n\t1 - умножение\n\t2 - деление\n\t3 - смешанные задания')
     my_choice = int(input('Ваш выбор: '))
     if my_choice == 1:
         multiplication()
     elif my_choice == 2:
         division()
+    elif my_choice == 3:
+        mix_tasks()
     else:
         print('Неверный ввод!')
 
